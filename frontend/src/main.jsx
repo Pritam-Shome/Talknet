57a3c0da9b67afc69d548c048c69f7d6a2dd300d
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2025-09-01 20:04:00 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 
 
 
-export const serverUrl="https://talknet-backend.onrender.com/"
+export const serverUrl="https://talknet-backend.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
